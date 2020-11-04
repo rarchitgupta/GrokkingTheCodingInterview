c7e2344f9cd68a666f96d891236473d578751542
Archit Gupta <rarchitgupta@gmail.com>
2020-11-04 15:18:43 +0530

    Minimum Size Sub Array Sum

---
+++ Sliding Window/MinSizeSubArraySum.cpp
@@ -0,0 +1,19 @@
+int findMinSubArray(int s, const vector<int> &arr)
+{
+    int minLength = INT_MAX;
+    int windowSum = 0;
+    int windowStart = 0;
+
+    for (int windowEnd = 0; windowEnd < arr.size(); windowEnd++)
+    {
+        windowSum += arr[windowEnd];
+        if (windowSum >= s)
+        {
+            minLength = min(minLength, windowEnd - windowStart + 1);
+            windowSum -= arr[windowStart];
+            windowStart++;
+        }
+    }
+
+    return minLength;
+}
